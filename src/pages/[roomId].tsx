74613304a98531aa47cbb62c919ed1ea4b5f27a0
Ben Thomas <ben@thomasnetwork.net>
2026-01-07 12:09:08 -0500

    Prompt for username before connecting to game

--- src/pages/[roomId].tsx
+++ src/pages/[roomId].tsx
@@ -7,23 +7,28 @@ import Layout from '@/components/Layout';
 import RoomCode from '@/components/RoomCode';
 import { useLocalStorage } from '@/hooks/useLocalStorage';
 import { getRandomUsername } from '@/lobby';
+import NameForm from '@/components/NameForm';
 
 export default function Home() {
   const router = useRouter();
   const [id] = useLocalStorage('id', uuid());
-  const [username, setUsername] = useLocalStorage(
-    'username',
-    getRandomUsername()
-  );
-
-  useEffect(() => {
-    if (username === '') setUsername(getRandomUsername());
-  }, [setUsername, username]);
+  const [username, setUsername] = useLocalStorage('username', '');
 
   if (!router.isReady || typeof router.query.roomId !== 'string') {
     return (
       <Layout>
         <div className="text-center">Loading...</div>
+      </Layout>
+    );
+  }
+
+  if (username === '') {
+    return (
+      <Layout>
+        <div>
+          <h1 className="pb-5 text-2xl">Cards</h1>
+          <NameForm {...{ username, setUsername }} />
+        </div>
       </Layout>
     );
   }
