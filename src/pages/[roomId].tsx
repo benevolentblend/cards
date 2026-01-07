0b3a663f75346c6be86968adac766647e86e4ccd
Ben Thomas <ben@thomasnetwork.net>
2026-01-07 12:14:04 -0500

    fixed linting

--- src/pages/[roomId].tsx
+++ src/pages/[roomId].tsx
@@ -1,13 +1,11 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import { v4 as uuid } from 'uuid';
 
 import Game from '@/components/Game';
 import Layout from '@/components/Layout';
+import NameForm from '@/components/NameForm';
 import RoomCode from '@/components/RoomCode';
 import { useLocalStorage } from '@/hooks/useLocalStorage';
-import { getRandomUsername } from '@/lobby';
-import NameForm from '@/components/NameForm';
 
 export default function Home() {
   const router = useRouter();
